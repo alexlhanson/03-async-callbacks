bec32ca1f7a2f3a28896948ac51051faf804fab7
alexlhanson <alexlhanson@gmail.com>
2018-09-09 17:11:41 -0700

    fixed tests not getting into callback yet for data

--- lab-alex/__test__/library.test.js
+++ lab-alex/__test__/library.test.js
@@ -4,10 +4,10 @@ jest.mock('fs');
 
 const reader = require('../lib/library.js');
 
-let file1 = `${__dirname}/../data.file1.txt`;
-let file2 = `${__dirname}/../data.file2.txt`;
-let file3 = `${__dirname}/../data.file3.txt`;
-let badFile = `${__dirname}/../data.bad.txt`;
+let file1 = `${__dirname}/../data/file1.txt`;
+let file2 = `${__dirname}/../data/file2.txt`;
+let file3 = `${__dirname}/../data/file3.txt`;
+let badFile = `${__dirname}/../data/bad.txt`;
 
 describe ('file reader module', () => {
 
--- lab-alex/lib/library.js
+++ lab-alex/lib/library.js
@@ -3,18 +3,22 @@
 const fs = require('fs');
 
 module.exports = exports = (pathsArr, callback) => {
+  //type guards
   if (!Array.isArray(pathsArr)){
     throw new Error('reader requires paths as an array');
   } else if (pathsArr.length !== 3){
     throw new Error('reader requires three filepaths');
   }
-
+  //resolving map of files
   let mappedFiles = pathsArr.map(file => {
     fs.readFile(file, (err, data) => {
-      if (err) {
+      if(err){
         callback(err);
       } else {
-        callback (null, data.toString());
+        setTimeout(() => {
+          console.log('here');
+          callback(null, data.toString().trim());
+        }, 0);
       }
     });
   });
